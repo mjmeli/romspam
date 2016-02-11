67ec131801a1eb65188fa68b07bfa94c62d2e87a
Michael Meli <mjmeli@ncsu.edu>
2016-02-10 20:32:39 -0500

    ability to get quote

---
+++ romspam/romquote.py
@@ -0,0 +1,22 @@
+from lxml import html
+import requests
+
+"""
+    getquote
+    Get a quote from the quote generating website and return it as a string.
+"""
+def getquote():
+    # Request the site
+    try:
+        page = requests.get('http://www.romanticlovemessages.com/random/')
+    except requests.exceptions.RequestException as e:
+        print "Could not request website. See log file for more info."
+        raise Exception("Could not request website. See log file for more info.")
+
+    # Retrieve the quote as an array of strings
+    tree = html.fromstring(page.content)
+    quotearr = tree.xpath('//div[@id="hl1"]/text()')
+
+    # Format the quote array properly into a single string
+    quote = '\n'.join(quotearr).replace('`', '\'')
+    return quote
--- romspam/tests/test_getquote.py
+++ romspam/tests/test_getquote.py
@@ -2,6 +2,8 @@ from unittest import TestCase
 from lxml import html
 import requests
 
+from romspam import romquote
+
 class TestGetQuote(TestCase):
     # Tests ability to get a random romantic quote
     def test_get_quote(self):
@@ -11,7 +13,15 @@ class TestGetQuote(TestCase):
         quote = tree.xpath('normalize-space(//div[@id="hl1"]/text())')
         button = tree.xpath('//center/form/button[@class="button"]/text()')
         # Verify this information
+        print "*****QUOTE****** = " + quote
         self.assertTrue(len(quote) >= 1)
         self.assertTrue(isinstance(quote, str))
         self.assertEqual(len(button), 1)
         self.assertEqual(button[0], "Next Random Romantic Love Message")
+
+    def test_get_quote_module(self):
+        for x in range(10):
+            quote = romquote.getquote()
+            self.assertTrue(len(quote) >= 1)
+            self.assertTrue(isinstance(quote, str))
+            self.assertFalse("`" in quote)
