107e1c181b4f358a1fb01a038dd920b51b8087d1
Michael <Meli@WHITEWOLF.local>
2016-07-28 22:14:33 -0400

    enhanced test image unit tests

--- romspam/tests/test_image.py
+++ romspam/tests/test_image.py
@@ -1,5 +1,6 @@
-from unittest import TestCase
 import os
+import mock
+from unittest import TestCase
 from romspam import romimage
 from romspam import romspam
 
@@ -10,7 +11,31 @@ class TestImage(TestCase):
         directory = os.path.join(romspam.rootLoc, "images")
         self.assertTrue(os.path.isdir(directory))
 
-        # Get image and check it exists
-        image = romimage.getimage(directory)
-        self.assertTrue(image.endswith(".png") or image.endswith(".jpg") or image.endswith(".gif") or f.endswith(".jpeg"))
-        self.assertTrue(os.path.isfile(image) or image == None)
+        # List to store retrieved images
+        images = []
+
+        # Get images
+        with mock.patch('os.listdir', return_value=['test.jpg']):
+            images.append(romimage.getimage(directory))
+        with mock.patch('os.listdir', return_value=['test.png']):
+            images.append(romimage.getimage(directory))
+        with mock.patch('os.listdir', return_value=['test.gif']):
+            images.append(romimage.getimage(directory))
+        with mock.patch('os.listdir', return_value=['test.jpeg']):
+            images.append(romimage.getimage(directory))
+
+        # Check each image passes
+        self.assertTrue(len(images) == 4)
+        for image in images:
+            self.assertTrue(image != None)
+            self.assertTrue(image.endswith(".png") or image.endswith(".jpg") or image.endswith(".gif") or image.endswith(".jpeg"))
+
+    # Test that getimage fails when there is no valid image available
+    def test_getimage_bad(self):
+        # Check if directory exists
+        directory = os.path.join(romspam.rootLoc, "images")
+        self.assertTrue(os.path.isdir(directory))
+
+        # Get bad image and check it exists
+        with mock.patch('os.listdir', return_value=['test.docx']):
+            self.assertTrue(romimage.getimage(directory) == None)
